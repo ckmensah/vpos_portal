021e6c02ac9f431045693da97b0d21381cacc82f
Nartey Francis Kweku <narteyf7@gmail.com>
2020-03-21 13:56:51 +0000

    Just after being done with sub_activity_index

--- app/controllers/sub_activity_masters_controller.rb
+++ app/controllers/sub_activity_masters_controller.rb
@@ -3,8 +3,20 @@ class SubActivityMastersController < ApplicationController
 
   # GET /sub_activity_masters
   # GET /sub_activity_masters.json
+
   def index
-    @sub_activity_masters = SubActivityMaster.all
+    params[:count] ? params[:count] : params[:count] = 50
+    params[:page] ? params[:page] : params[:page] = 1
+
+    @sub_activity_masters = SubActivityMaster.paginate(:page => params[:page], :per_page => params[:count]).order('created_at desc')
+  end
+
+
+  def sub_activity_master_index
+    params[:count] ? params[:count] : params[:count] = 50
+    params[:page] ? params[:page] : params[:page] = 1
+
+    @sub_activity_masters = SubActivityMaster.paginate(:page => params[:page], :per_page => params[:count]).order('created_at desc')
   end
 
   # GET /sub_activity_masters/1
@@ -29,9 +41,12 @@ class SubActivityMastersController < ApplicationController
     respond_to do |format|
       if @sub_activity_master.save
         format.html { redirect_to @sub_activity_master, notice: 'Sub activity master was successfully created.' }
+        flash.now[:notice] = "Activity Code was successfully created."
+        format.js { render :show }
         format.json { render :show, status: :created, location: @sub_activity_master }
       else
         format.html { render :new }
+        format.js { render :new }
         format.json { render json: @sub_activity_master.errors, status: :unprocessable_entity }
       end
     end
@@ -43,9 +58,12 @@ class SubActivityMastersController < ApplicationController
     respond_to do |format|
       if @sub_activity_master.update(sub_activity_master_params)
         format.html { redirect_to @sub_activity_master, notice: 'Sub activity master was successfully updated.' }
+        flash.now[:notice] = "Activity Code was successfully updated."
+        format.js { render :show }
         format.json { render :show, status: :ok, location: @sub_activity_master }
       else
         format.html { render :edit }
+        format.js { render :edit }
         format.json { render json: @sub_activity_master.errors, status: :unprocessable_entity }
       end
     end
@@ -53,18 +71,42 @@ class SubActivityMastersController < ApplicationController
 
   # DELETE /sub_activity_masters/1
   # DELETE /sub_activity_masters/1.json
+
   def destroy
-    @sub_activity_master.destroy
-    respond_to do |format|
-      format.html { redirect_to sub_activity_masters_url, notice: 'Sub activity master was successfully destroyed.' }
-      format.json { head :no_content }
+    params[:count] ? params[:count] : params[:count] = 50
+    params[:page] ? params[:page] : params[:page] = 1
+
+    if @sub_activity_master.active_status
+      @sub_activity_master.active_status = false
+      @sub_activity_master.save(validate: false)
+      @sub_activity_masters = SubActivityMaster.paginate(:page => params[:page], :per_page => params[:count]).order('created_at desc')
+      respond_to do |format|
+        format.html { redirect_to activity_types_url, notice: 'Occupation master was successfully disabled.' }
+        flash.now[:note] = 'Activity Code was successfully disabled.'
+        format.js { render :layout => false}
+        format.json { head :no_content }
+        # window.location.href = "<%= recipe_path(@recipe) %>"
+      end
+
+    else
+      @sub_activity_master.active_status = true
+      @sub_activity_master.save(validate: false)
+      @sub_activity_masters = SubActivityMaster.paginate(:page => params[:page], :per_page => params[:count]).order('created_at desc')
+      respond_to do |format|
+        format.html { redirect_to activity_types_url, notice: 'Allergy master was successfully enabled.' }
+        flash.now[:notice] = 'Activity Code was successfully enabled.'
+        format.js { render :layout => false }
+        format.json { head :no_content }
+      end
     end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_sub_activity_master
-      @sub_activity_master = SubActivityMaster.find(params[:id])
+      #@sub_activity_master = SubActivityMaster.find(params[:id])
+      @sub_activity_master = SubActivityMaster.where(assigned_code: params[:id]).order('id desc').first
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
