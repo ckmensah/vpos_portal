22df3439a74b41c88d200e7070fa2b7d5f9b4268
Nartey Francis Kweku <narteyf7@gmail.com>
2020-03-31 15:18:14 +0000

    I worked on merchant email validation.

--- app/models/entity_info_extra.rb
+++ app/models/entity_info_extra.rb
@@ -4,14 +4,16 @@ class EntityInfoExtra < ApplicationRecord
 
   validates :contact_number, presence: {message: " cannot be empty"}, format: { with: /\A\d+\z/, message: "must be numbers only." }#, uniqueness: { message: "Momo Number has already been set up." } #numericality: { only_integer: {message: "Please enter only numbers"} }
   #validates :web_url, presence: {message: " cannot be empty."}
-  validates :contact_email, presence: {message: " cannot be empty."}
-  validate :email_probably_valid
+  validates :contact_email, presence: {message: " cannot be empty."}, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i, message: " format is invalid." }
+  #validate :email_probably_valid
   validate :web_probably_valid
 
   def email_probably_valid
     valid = '[A-Za-z\d.+-]+' #Commonly encountered email address characters
-    check = (self.contact_email =~ /#{valid}@#{valid}\.#{valid}/) == 0
-    unless check
+    check1 = (self.contact_email =~ /#{valid}@#{valid}\.#{valid}/) == 0
+    check2 = (self.contact_email =~ /#{valid}@#{valid}\.#{valid}\.#{valid}/) == 0
+    if check1 || check2
+    else
       errors.add :contact_email, " format is invalid."
     end
 
