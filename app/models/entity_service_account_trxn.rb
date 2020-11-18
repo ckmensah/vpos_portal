792b9968f24c5e46d31f596d6cdea478e93b9316
Francis Nartey <francis@appsnmobilesolutions.com>
2020-11-18 22:55:11 +0000

    First release wallet statement 2

--- app/models/entity_service_account_trxn.rb
+++ app/models/entity_service_account_trxn.rb
@@ -21,8 +21,8 @@ class EntityServiceAccountTrxn < ApplicationRecord
     balance_bf = 0.000
     @entity_service_trxn_cb = EntityServiceAccountTrxn.where("entity_div_code = '#{division_code}' and created_at between '#{start_date} 00:00:00' and '#{end_date} 23:59:59'").order(created_at: :desc).first
     @entity_service_trxn_bbf = EntityServiceAccountTrxn.where("entity_div_code =' #{division_code}' and created_at < '#{start_date} 00:00:00'").order(created_at: :desc).first
-    closing_bal = (@entity_service_trxn_cb.net_bal_aft - @entity_service_trxn_cb.net_bal_bef).round(3) if @entity_service_trxn_cb
-    balance_bf = (@entity_service_trxn_bbf.net_bal_aft - @entity_service_trxn_bbf.net_bal_bef).round(3) if @entity_service_trxn_bbf
+    closing_bal = @entity_service_trxn_cb.net_bal_aft.round(3) if @entity_service_trxn_cb
+    balance_bf = @entity_service_trxn_bbf.net_bal_aft.round(3) if @entity_service_trxn_bbf
     return balance_bf, closing_bal
   end
 end
